e785ba8766d365e25471d5488cc72bcd3e8b5e86
Paweł Żulewski <pazulewski@gmail.com>
2021-11-20 00:09:28 +0100

    add entity Comment

--- src/main/java/eu/zulewski/entity/Comment.java
+++ src/main/java/eu/zulewski/entity/Comment.java
@@ -1,4 +1,26 @@
 package eu.zulewski.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
