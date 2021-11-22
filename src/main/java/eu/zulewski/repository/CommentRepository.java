170a826076a9df3de0513549bb4b56365d7eb1c4
Paweł Żulewski <pazulewski@gmail.com>
2021-11-22 23:36:33 +0100

    add repositories

---
+++ src/main/java/eu/zulewski/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package eu.zulewski.repository;
+
+import eu.zulewski.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
---
+++ src/main/java/eu/zulewski/repository/SubredditRepository.java
@@ -0,0 +1,9 @@
+package eu.zulewski.repository;
+
+import eu.zulewski.entity.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+}
