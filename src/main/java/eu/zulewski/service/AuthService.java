d12d82c3834a45b322e618589357fb8ce9a95010
Paweł Żulewski <pazulewski@gmail.com>
2021-11-24 23:57:01 +0100

    add generateVerificationToken method

--- src/main/java/eu/zulewski/service/AuthService.java
+++ src/main/java/eu/zulewski/service/AuthService.java
@@ -2,19 +2,47 @@ package eu.zulewski.service;
 
 import eu.zulewski.dto.RegisterRequest;
 import eu.zulewski.entity.User;
+import eu.zulewski.entity.VerificationToken;
+import eu.zulewski.repository.UserRepository;
+import eu.zulewski.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
     public void singup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
+        userRepository.save(user);
+
+        generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
     }
 }
